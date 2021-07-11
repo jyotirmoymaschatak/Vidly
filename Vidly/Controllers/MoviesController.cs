6e3df3fb91643ebc97dba24a5f58b49ff214f904
jmaschatak1 <38112743+jmaschatak1@users.noreply.github.com>
2021-07-11 23:51:39 +0530

    Action Parameters, Covention based routing, Attribute routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,8 +12,38 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {Name="Alpha Male!" };
-            return View(movie);
+            var movie = new Movie() { Name = "Alpha Male!" };
+
+            //return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
+        }
+
+        //Get: Movies/Edit/1
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // Get: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        //Get Movies/released/2015/4
+        [Route("Movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month); 
         }
     }
 }
